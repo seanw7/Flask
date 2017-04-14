80bdc29dfe9181c08e2ddb649ebc2df58d4078d6
Sean Wilkie <seanw@protonmail.ch>
2017-04-14 13:05:23 -0700

    made some changes, added link in homepage to twitter login. Also made before request function that saves users login info in a g.user object

--- python/app.py
+++ python/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, session, redirect, request, url_for, g
 from twitter_utils import get_request_token, get_oauth_verifier_url, get_access_token
 from user import User
 from database import Database
@@ -7,6 +7,11 @@ app = Flask(__name__)
 app.secret_key = '1234'
 
 Database.initialise(database='postgres', user='postgres', password='Ac3TeX411', host='localhost')
+
+@app.before_request
+def load_user():
+    if 'screen_name' in session:
+        g.user = User.load_from_db_by_screen_name(session['screen_name'])
 
 
 @app.route('/')
@@ -21,6 +26,11 @@ def twitter_login():
     # sets the call back url to : "http://127.0.0.1:4995/auth/t witter"
     return redirect(get_oauth_verifier_url(request_token))
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('homepage'))
+
 @app.route('/auth/twitter') #  http://127.0.0.1:4995/auth/twitter?oauth_verifier=
 def twitter_auth():
     oauth_verifier = request.args.get('oauth_verifier')
@@ -33,11 +43,11 @@ def twitter_auth():
         user.save_to_db()
     # this will remember the users screen name with a cookie
     session['screen_name'] = user.screen_name
-    return user.screen_name
+    return redirect(url_for('profile'))
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html', screen_name=session['screen_name'])
+    return render_template('profile.html', user=g.user)
 
 
 
